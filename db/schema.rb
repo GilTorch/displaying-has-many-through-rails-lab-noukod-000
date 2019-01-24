edcbbed0dcb338919f874568e0b29b98a26da887
Gilbert Torchon <gilbert@noukod.com>
2019-01-24 20:33:21 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190124194840) do
+ActiveRecord::Schema.define(version: 20190124203251) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer  "doctor_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20190124194840) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "department"
   end
 
   create_table "patients", force: :cascade do |t|
